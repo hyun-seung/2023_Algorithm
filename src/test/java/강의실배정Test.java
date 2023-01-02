beb8f09a8ceb287e1b520dc8d4dd29e7790bc181
hyun-seung <ardimento22@naver.com>
2023-01-02 20:52:45 +0900

    [TEST] 백준_강의실배정_11000

---
+++ src/test/java/강의실배정Test.java
@@ -0,0 +1,40 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.greedy.강의실배정_11000;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 강의실배정Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "3\n1 3\n2 4\n3 5"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        강의실배정_11000 test = new 강의실배정_11000();
+        test.main(new String[]{});
+
+        Assertions.assertEquals(result, testSetting.getOutput());
+    }
+}

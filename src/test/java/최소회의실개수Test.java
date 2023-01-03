93f30f7ae3a571da699fc6180fee746bba348b60
hyun-seung <ardimento22@naver.com>
2023-01-03 23:42:02 +0900

    [TEST] 백준_최소회의실개수_19598

---
+++ src/test/java/최소회의실개수Test.java
@@ -0,0 +1,41 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.greedy.최소회의실개수_19598;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 최소회의실개수Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "3\n0 40\n15 30\n5 10",
+                "2\n10 20\n5 10"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2", "1"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        최소회의실개수_19598 test = new 최소회의실개수_19598();
+        test.main(new String[]{});
+
+        Assertions.assertEquals(result, testSetting.getOutput());
+    }
+}

a5f62e65197a925e5ae083cdb146b58775108cc4
hyun-seung <ardimento22@naver.com>
2023-01-10 03:04:21 +0900

    [TEST] 백준_큰 수_18511

---
+++ src/test/java/큰수Test.java
@@ -0,0 +1,46 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.burte_force.큰수_18511;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 큰수Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "657 3\n1 5 7"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"577"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        큰수_18511 test = new 큰수_18511();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+//        String[] outputArray = output.split("\n");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(outputArray[0], outputArray[1]);
+        Assertions.assertEquals(result, output);
+    }
+}

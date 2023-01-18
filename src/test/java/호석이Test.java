40373fe71d8c68c9c47e418e03b35cb76f73f244
hyun-seung <ardimento22@naver.com>
2023-01-19 04:04:41 +0900

    [TEST] 백준_호석이_21278

---
+++ src/test/java/호석이Test.java
@@ -0,0 +1,51 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.burte_force.호석이_21278;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 호석이Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "5 4\n"
+                        + "1 3\n"
+                        + "4 2\n"
+                        + "2 5\n"
+                        + "3 2"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 2 6"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        호석이_21278 test = new 호석이_21278();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+
+//        String[] resultArray = result.split(" ");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(resultArray[0], resultArray[1], resultArray[2]);
+        Assertions.assertEquals(result, output);
+    }
+}

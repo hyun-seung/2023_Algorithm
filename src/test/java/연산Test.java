1974417b2da902f431eb9cc6001333fa3a4960d4
hyun-seung <ardimento22@naver.com>
2023-01-22 23:04:51 +0900

    [TEST] 백준_연산_21943

---
+++ src/test/java/연산Test.java
@@ -0,0 +1,55 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.burte_force.연산_21943;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 연산Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "6\n"
+                        + "1 2 4 5 7 8\n"
+                        + "4 1",
+                "3\n"
+                        + "1 9 1\n"
+                        + "2 0",
+                "5\n"
+                        + "1 2 3 4 5\n"
+                        + "1 3"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"182", "11", "180"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        연산_21943 test = new 연산_21943();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+
+//        String[] resultArray = result.split(" ");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(resultArray[0], resultArray[1], resultArray[2]);
+        Assertions.assertEquals(result, output);
+    }
+}

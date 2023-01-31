f3b6182f9d92714f575d625b94c083244d767e95
hyun-seung <ardimento22@naver.com>
2023-01-31 20:44:42 +0900

    [TEST] 백준_토마토_7569

---
+++ src/test/java/토마토7569Test.java
@@ -0,0 +1,64 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.graph.토마토_7569;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 토마토7569Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "5 3 1\n"
+                        + "0 -1 0 0 0\n"
+                        + "-1 -1 0 1 1\n"
+                        + "0 0 0 1 1",
+                "5 3 2\n"
+                        + "0 0 0 0 0\n"
+                        + "0 0 0 0 0\n"
+                        + "0 0 0 0 0\n"
+                        + "0 0 0 0 0\n"
+                        + "0 0 1 0 0\n"
+                        + "0 0 0 0 0",
+                "4 3 2\n"
+                        + "1 1 1 1\n"
+                        + "1 1 1 1\n"
+                        + "1 1 1 1\n"
+                        + "1 1 1 1\n"
+                        + "-1 -1 -1 -1\n"
+                        + "1 1 1 -1"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1", "4", "0"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        토마토_7569 test = new 토마토_7569();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+
+//        String[] resultArray = result.split(" ");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(resultArray[0], resultArray[1], resultArray[2]);
+        Assertions.assertEquals(result, output.trim());
+    }
+}

26d4cee647aa0cb46ff7cce501d86f772bbbbb2e
hyun-seung <ardimento22@naver.com>
2023-02-01 16:21:06 +0900

    [TEST] 백준_숨바꼭질_13549

---
+++ src/test/java/숨바꼭질Test.java
@@ -0,0 +1,47 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.graph.숨바꼭질_13549;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 숨바꼭질Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "5 17"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        숨바꼭질_13549 test = new 숨바꼭질_13549();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+
+//        String[] resultArray = result.split(" ");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(resultArray[0], resultArray[1], resultArray[2]);
+        Assertions.assertEquals(result, output.trim());
+    }
+}

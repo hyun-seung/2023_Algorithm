fcfa4c18b81bb0648fb0a285fb3b711e9625e775
hyun-seung <ardimento22@naver.com>
2023-02-03 23:11:42 +0900

    [TEST] 백준_일루미네이션_5547

---
+++ src/test/java/일루미네이션Test.java
@@ -0,0 +1,57 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.graph.일루미네이션_5547;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 일루미네이션Test {
+
+    TestSetting testSetting = new TestSetting();
+
+    @BeforeEach
+    void setUp() {
+        testSetting.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testSetting.outputStream));
+    }
+
+    public InputStream settingInputs() {
+        List<String> inputs = List.of(
+                "8 4\n"
+                        + "0 1 0 1 0 1 1 1\n"
+                        + "0 1 1 0 0 1 0 0\n"
+                        + "1 0 1 0 1 1 1 1\n"
+                        + "0 1 1 0 1 0 1 0",
+                "8 5\n"
+                        + "0 1 1 1 0 1 1 1\n"
+                        + "0 1 0 0 1 1 0 0\n"
+                        + "1 0 0 1 1 1 1 1\n"
+                        + "0 1 0 1 1 0 1 0\n"
+                        + "0 1 1 0 1 1 0 0"
+        );
+
+        return testSetting.createInputStream(inputs);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"64", "56"})
+    void test(String result) throws IOException {
+        System.setIn(settingInputs());
+
+        일루미네이션_5547 test = new 일루미네이션_5547();
+        test.main(new String[]{});
+
+        String output = testSetting.getOutput();
+        output = output.replaceAll(System.getProperty("line.separator"), "");
+
+//        String[] resultArray = result.split(" ");
+
+//        org.assertj.core.api.Assertions.assertThat(output)
+//                .contains(resultArray[0], resultArray[1], resultArray[2]);
+        Assertions.assertEquals(result, output.trim());
+    }
+}

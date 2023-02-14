105fd35b3c8fc958a1b3e01f037557fa59c46048
hyun-seung <ardimento22@naver.com>
2023-02-14 11:04:52 +0900

    [FEAT] 백준_나무자르기_2805

---
+++ src/main/java/org/binary/나무자르기_2805.java
@@ -0,0 +1,57 @@
+package org.binary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 나무자르기_2805 {
+
+    private static int N;
+    private static int M;
+    private static int[] arr;
+
+    private static long max;
+
+    public static void main(String[] args) throws IOException {
+        setBase();
+
+        long start = 0;
+        long end = max;
+
+        while(start <= end) {
+            long mid = (start + end) / 2;
+            long sum = 0;
+
+            for(int i : arr) {
+                if(i > mid) {
+                    sum += i - mid;
+                }
+            }
+
+            if(sum >= M) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+    }
+
+    private static void setBase() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        max = Long.MIN_VALUE;
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+    }
+}

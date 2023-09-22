6c47a60ccad4d07f056e85258231172b0b03610f
hyunseung <ardimento22@naver.com>
2023-09-22 21:22:51 +0900

    [프로그래머스] 테이블 해시 함수

---
+++ src/main/java/org/implementation/테이블해시함수.java
@@ -0,0 +1,23 @@
+package org.implementation;
+
+import java.util.Arrays;
+
+public class 테이블해시함수 {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+
+        Arrays.sort(data, ((o1, o2) -> o1[col - 1] != o2[col - 1] ? o1[col - 1] - o2[col - 1] : o2[0] - o1[0]));
+
+        for (int i = row_begin - 1; i <= row_end - 1; i++) {
+            int S_i = 0;
+
+            for (int d : data[i]) {
+                S_i += (d % (i + 1));
+            }
+
+            answer = (answer ^ S_i);
+        }
+
+        return answer;
+    }
+}
